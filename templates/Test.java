fd739ff40b211411a585b4b00b04f4e7b40cd654
liucong <liucong@rigongyi.cn>
2022-07-04 16:29:12 +0800

    chore: 初始化项目，添加 Application Test 模板

---
+++ templates/Test.java
@@ -0,0 +1,11 @@
+package {{pkgName}};
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class {{applicationClassName}}Tests {
+
+  @Test
+  void contextLoads() {}
+}
