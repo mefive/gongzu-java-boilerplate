598d35d295c74392fd035245ae1f9ada2321cf3c
liucong <liucong@rigongyi.cn>
2022-07-04 20:01:23 +0800

    chore: 更新 entity template

--- templates/entity-suit/entity/Entity.java
+++ templates/entity-suit/entity/Entity.java
@@ -1,1 +1,28 @@
-123
+package {{pkgName}};
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "{{tableName}}")
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+public class {{entityClass}} {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    {{#each fileds}}
+    private {{this.type}} {{this.name}};
+
+    {{/each}}
+}
