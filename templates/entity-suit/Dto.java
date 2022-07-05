1f2d01d4290925660bd5b0f1533e8cd738295b82
liucong <liucong@rigongyi.cn>
2022-07-05 14:43:31 +0800

    feat: dto 增加依赖

--- templates/entity-suit/Dto.java
+++ templates/entity-suit/Dto.java
@@ -1,8 +1,12 @@
 package {{pkgName}}.dto;
 
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.Data;
+
 @Data
 public class {{entityClassName}}Dto implements Serializable {
-
 	{{#each fields}}
 
 	private {{this.type}} {{this.name}};
