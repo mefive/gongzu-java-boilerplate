3bca674163f1cfb0457d225da9ccef35113c8d95
liucong <liucong@rigongyi.cn>
2022-07-05 22:53:33 +0800

    feat: 实现 service 模板

--- templates/entity-suit/Service.java
+++ templates/entity-suit/Service.java
@@ -1,1 +1,38 @@
-123
+package {{pkgName}}
+
+@Service
+public class {{entityClassName}}Service {
+
+	private final {{entityClassName}}Repository {{entityName}}Repository;
+
+	private final {{entityClassName}}Mapper = Mappers.getMapper({{entityClassName}}Mapper.class);
+
+	public {{entityClassName}}Service({{entityClassName}}Repository {{entityName}}Repository) {
+		this.{{entityName}}Repository =  {{entityName}}Repository;
+	}
+
+	@Transactional
+	public {{entityClassName}} save({{entityClassName}}Dto {{entityName}}Dto) {
+		{{entityClassName}} {{entityName}} = {{entityName}}Mapper.{{entityName}}DtoTo{{entityClassName}}({{entityName}}Dto);
+		{{entityName}}Repository.save({{entityName}});
+		return {{entityName}};
+	}
+
+	@Transactional
+	public {{entityClassName}} update(Integer id, {{entityClassName}}Dto {{entityName}}Dto) throws Exception {
+		{{entityClassName}} {{entityName}} = {{entityName}}Repository.findById(id)
+            .orElseThrow(() -> new Exception("未找到{{entityNameCh}}"));
+
+        {{entityName}}Mapper.update{{entityClassName}}From{{entityClassName}}Dto({{entityName}}Dto, {{entityName}});
+        {{entityName}}Repository.save({{entityName}});
+
+        return {{entityName}};
+	}
+
+    @Transactional
+    public void remove(Integer id) throws Exception {
+		{{entityClassName}} {{entityName}} = {{entityName}}Repository.findById(id)
+            .orElseThrow(() -> new Exception("未找到{{entityNameCh}}"));
+        {{entityName}}Repository.delete({{entityName}});
+    }
+}
