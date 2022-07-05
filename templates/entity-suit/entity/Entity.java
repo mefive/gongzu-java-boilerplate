0c9aad6b7841d6082028b759e52f74bf9948618b
liucong <liucong@rigongyi.cn>
2022-07-05 14:14:20 +0800

    feat: 实体类增加注解注入

--- templates/entity-suit/entity/Entity.java
+++ templates/entity-suit/entity/Entity.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,12 +18,11 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @NoArgsConstructor
 public class {{entityClassName}} {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
     {{#each fields}}
 
+    {{#each annotations}}
+    {{this}}
+    {{/each}}
     private {{this.type}} {{this.name}};
     {{/each}}
 }
